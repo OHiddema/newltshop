f593f50b267960068852bc6d2db79b79583daff1
Oege Hiddema <oegehiddema@gmail.com>
2019-06-13 19:57:16 +0200

    use layout file on all views

--- resources/views/laptops/create.blade.php
+++ resources/views/laptops/create.blade.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <title>Document</title>
-</head>
-<body>
-   <h1>Add a laptop to the shop</h1>
+@extends('layout')
+
+@section('content')
+<h1>Add a laptop to the shop</h1>
    <form method="POST" action="/laptops">
       {{csrf_field()}}
       <label for="brand">Brand: </label>
@@ -24,5 +18,4 @@
       
       <button type="submit">Add laptop</button>
    </form>
-</body>
-</html>
+@endsection
--- resources/views/laptops/index.blade.php
+++ resources/views/laptops/index.blade.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <title>Document</title>
-</head>
-<body>
-   <h1>Laptops</h1>
+@extends('layout')
+
+@section('content')
+<h1>Laptops</h1>
    <ul>
       @foreach($laptops as $laptop)
       <a href="/laptops/{{$laptop->id}}">
@@ -15,5 +9,4 @@
       </a>
       @endforeach
    </ul>
-</body>
-</html>
+@endsection
